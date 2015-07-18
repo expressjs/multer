9205463cfabcc3c5afc9d13cea261e69ef6fc865
Linus Unnebäck <linus@folkdatorn.se>
2015-07-18 12:13:39 +0200

    test: unicode

---
+++ test/unicode.js
@@ -0,0 +1,56 @@
+/* eslint-env mocha */
+
+var assert = require('assert')
+
+var path = require('path')
+var util = require('./_util')
+var multer = require('../')
+var temp = require('fs-temp')
+var rimraf = require('rimraf')
+var FormData = require('form-data')
+
+describe('Unicode', function () {
+  var uploadDir, upload
+
+  beforeEach(function (done) {
+    temp.mkdir(function (err, path) {
+      if (err) return done(err)
+
+      var storage = multer.diskStorage({
+        destination: path,
+        filename: function (req, file, cb) {
+          cb(null, file.originalname)
+        }
+      })
+
+      uploadDir = path
+      upload = multer({ storage: storage })
+      done()
+    })
+  })
+
+  afterEach(function (done) {
+    rimraf(uploadDir, done)
+  })
+
+  it('should handle unicode filenames', function (done) {
+    var form = new FormData()
+    var parser = upload.single('small0')
+    var filename = '\ud83d\udca9.dat'
+
+    form.append('small0', util.file('small0.dat'), { filename: filename })
+
+    util.submitForm(parser, form, function (err, req) {
+      assert.ifError(err)
+
+      assert.equal(path.basename(req.file.path), filename)
+      assert.equal(req.file.originalname, filename)
+
+      assert.equal(req.file.fieldname, 'small0')
+      assert.equal(req.file.size, 1778)
+      assert.equal(util.fileSize(req.file.path), 1778)
+
+      done()
+    })
+  })
+})
