4b71c6226ce465a1b56454713970f275adf719fe
pavel <pavel.github@outlook.com>
2016-08-04 06:47:47 -0400

    Test fixes

--- test/none.js
+++ test/none.js
@@ -22,7 +22,22 @@ describe('None', function () {
 
     util.submitForm(parser, form, function (err, req) {
       // "Unexpected field" error is expected
-      assert.ok(err)
+      assert.equal(err.code, 'LIMIT_UNEXPECTED_FILE')
+      assert.deepEqual(req.files, {})
+      assert.equal(req.body['key1'], 'val1')
+      assert.equal(req.body['key2'], 'val2')
+      done()
+    })
+  })
+
+  it('should handle text fields', function (done) {
+    var form = new FormData()
+
+    form.append('key1', 'val1')
+    form.append('key2', 'val2')
+
+    util.submitForm(parser, form, function (err, req) {
+      assert.ifError(err)
       assert.deepEqual(req.files, {})
       assert.equal(req.body['key1'], 'val1')
       assert.equal(req.body['key2'], 'val2')
