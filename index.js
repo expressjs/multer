b32ab3006415ec341f488b2ce4151a677f2fb7fa
eMAD <emad.alam@construyo.de>
2019-10-22 13:30:32 +0200

    Support startProcessing option for custom handling

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function allowAll (req, file, cb) {
   cb(null, true)
 }
 
+function startProcessing (req, busboy) {
+  req.pipe(busboy)
+}
+
 function Multer (options) {
   if (options.storage) {
     this.storage = options.storage
@@ -20,6 +24,7 @@ function Multer (options) {
   this.limits = options.limits
   this.preservePath = options.preservePath
   this.fileFilter = options.fileFilter || allowAll
+  this.startProcessing = options.startProcessing || startProcessing
 }
 
 Multer.prototype._makeMiddleware = function (fields, fileStrategy) {
@@ -49,7 +54,8 @@ Multer.prototype._makeMiddleware = function (fields, fileStrategy) {
       preservePath: this.preservePath,
       storage: this.storage,
       fileFilter: wrappedFileFilter,
-      fileStrategy: fileStrategy
+      fileStrategy: fileStrategy,
+      startProcessing: this.startProcessing
     }
   }
 
@@ -79,7 +85,8 @@ Multer.prototype.any = function () {
       preservePath: this.preservePath,
       storage: this.storage,
       fileFilter: this.fileFilter,
-      fileStrategy: 'ARRAY'
+      fileStrategy: 'ARRAY',
+      startProcessing: this.startProcessing
     }
   }
 
