83a772171d9ef1c0c4b56cd82f47a01dc6613c65
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-08-10 22:48:27 +0200

    chore: increase test coverage

--- test/error-handling.js
+++ test/error-handling.js
@@ -459,4 +459,20 @@ describe('Error Handling', function () {
       done()
     })
   })
+
+  it('should throw TypeError if options is not an object', function () {
+    assert.throws(() => {
+      multer(null)
+    }, {
+      name: 'TypeError',
+      message: 'Expected object for argument options'
+    })
+
+    assert.throws(() => {
+      multer('invalid')
+    }, {
+      name: 'TypeError',
+      message: 'Expected object for argument options'
+    })
+  })
 })
