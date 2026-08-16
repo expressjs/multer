0d10d3ead56e2b4ad7b242c25176f91d915d40d4
sahilraut191685 <sahilraut200619@gmail.com>
2026-08-17 01:06:31 +0530

    fix: add fallback for unknown error codes

--- test/error-handling.js
+++ test/error-handling.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 
 var assert = require('assert')
-
+var MulterError = require('../lib/multer-error')
 var os = require('os')
 var util = require('./_util')
 var multer = require('../')
@@ -11,12 +11,20 @@ var FormData = require('form-data')
 var http = require('http')
 var net = require('net')
 
-function withLimits (limits, fields) {
+function withLimits(limits, fields) {
   var storage = multer.memoryStorage()
   return multer({ storage: storage, limits: limits }).fields(fields)
 }
 
 describe('Error Handling', function () {
+
+
+  it('should use a fallback message when the code is not mapped', function () {
+    var err = new MulterError('SOME_NEW_CODE')
+    assert.strictEqual(err.message, 'Unknown error: SOME_NEW_CODE')
+  })
+
+
   it('should be an instance of both `Error` and `MulterError` classes in case of the Multer\'s error', function (done) {
     var form = new FormData()
     var storage = multer.diskStorage({ destination: os.tmpdir() })
@@ -194,7 +202,7 @@ describe('Error Handling', function () {
   it('should report errors from storage engines', function (done) {
     var storage = multer.memoryStorage()
 
-    storage._removeFile = function _removeFile (req, file, cb) {
+    storage._removeFile = function _removeFile(req, file, cb) {
       var err = new Error('Test error')
       err.code = 'TEST'
       cb(err)
@@ -324,7 +332,7 @@ describe('Error Handling', function () {
         })
       }, 8000)
 
-      function finish (err) {
+      function finish(err) {
         if (finished) return
         finished = true
         clearTimeout(timeout)
@@ -333,7 +341,7 @@ describe('Error Handling', function () {
         })
       }
 
-      function writeChunk () {
+      function writeChunk() {
         if (sentChunks >= totalChunks) {
           sock.write(footer)
           return
@@ -429,7 +437,7 @@ describe('Error Handling', function () {
         }, 50)
       })
 
-      sock.on('error', function () {})
+      sock.on('error', function () { })
     })
   })
 
@@ -448,7 +456,7 @@ describe('Error Handling', function () {
       uploadedFiles.push({ fieldname: 'file', originalname: 'f.dat', buffer: Buffer.alloc(0) })
     }
 
-    function syncRemove (file, cb) {
+    function syncRemove(file, cb) {
       delete file.buffer
       cb(null)
     }
